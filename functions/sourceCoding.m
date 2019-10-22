af53cf81fc7952b139dba56e50affb97c6e72408
Cleverson Nahum <cleversonahum@gmail.com>
2019-10-22 14:31:35 -0300

    rounding and casting valuesfrom DCT

--- functions/sourceCoding.m
+++ functions/sourceCoding.m
@@ -5,8 +5,11 @@ function [ comp_sig ] = sourceCoding( image_path )
     
     [syms, p] = imagesHuffman('images'); % Defining huffman requirements
     dict = huffmandict(syms, p); % Huffman Dictionary
-    sig = uint8(dct2(single(imread(image_path))));
-    comp_sig = huffmanenco(sig(:),dict);
+    dct_img = dct2(single(imread(image_path))); % DCT value from one image
+    dct_img(abs(dct_img)<20) = 0; % Discarding high frequencies with low values
+    dct_img = round(dct_img); % Round values (avoiding fractional)
+    dct_img = int16(dct_img); % Converting to uint16 representation
+    comp_sig = huffmanenco(dct_img(:),dict); % Encoding signal in huffman representation
 
 end
 
