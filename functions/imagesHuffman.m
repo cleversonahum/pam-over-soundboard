a12e9b734f247b04825561df2aa2e5ebd9028b37
Cleverson Nahum <cleversonahum@gmail.com>
2019-10-22 12:07:41 -0300

    rounding and converting values to int16

--- functions/imagesHuffman.m
+++ functions/imagesHuffman.m
@@ -7,8 +7,11 @@ function [ syms, p ] = imagesHuffman( images_path )
     images = dir([images_path filesep '*.tiff']);
     dct_imgs = [];
     for i = 1:length(images)
-       dct_img = uint8(dct(single(imread([images_path filesep...
-           images(i).name])))); % DCT value from one image
+       dct_img = dct2(single(imread([images_path filesep...
+           images(i).name]))); % DCT value from one image
+       dct_img(abs(dct_img)<20) = 0; % Discarding high frequencies with low values
+       dct_img = round(dct_img); % Round values (avoiding fractional)
+       dct_img = int16(dct_img); % Converting to uint16 representation
        dct_imgs = [dct_imgs; dct_img(:)];
     end
     dct_imgs = sort(dct_imgs);
