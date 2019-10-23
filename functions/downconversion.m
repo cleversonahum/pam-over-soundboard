58d8c405c64c4d3429cc32b2dc3345a49b072eb4
PedroBemerguy <pedrobemerguy96@gmail.com>
2019-10-23 10:15:09 -0300

    Fix bug at downconverted function

--- functions/downconversion.m
+++ functions/downconversion.m
@@ -1,9 +1,9 @@
-function [ upconvertedSignal ] = upconversion( signal, frequency, Fs)
+function [ downconvertedSignal ] = downconversion( signal, frequency, Fs)
 %upconversion Summary of this function goes here
 %   This function translate a  signal in DC to a band pass frequency
  
     n = 0:length(signal)-1;
-    upconvertedSignal = signal .* cos(2*pi*frequency*(n/Fs));
+    downconvertedSignal = signal .* cos(2*pi*frequency*(n/Fs));
 
 
 end
