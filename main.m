30a809fffbbd85a497456b48cb755a190b18374b
Cleverson Nahum <cleversonahum@gmail.com>
2019-10-23 09:58:15 -0300

    removing comments

--- main.m
+++ main.m
@@ -1,15 +1,5 @@
-% Steps to make the transmission description
+% PAM Transmission simulation where channel is simulated
 
-% TRANSMISSION/RECEPTION
-% 1 - convert image to binary representation / from binary to image representation
-% 2 - convert binary representation to M-PAM symbols / from M-PAM symbols to binary
-% 3 - Upsampling (consider baud rate) / Downsampling
-% 4 - from upsampled symbols to frames with preambles / from frames to upsampled symbols without preable, sync
-% 5 - convolution with shaping pulse (maybe use filter) / matched filter, signal gain, Automatic gain control
-% 6 - upconversion / downconversion
-% 7 - from symbols to audio / from audio to symbols
-
-% Observation: consider relation between BW, symbol rate and Fs. Equalizer
 addpath('functions');
 
 
