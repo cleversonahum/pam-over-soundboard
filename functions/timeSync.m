d76435639e81a519be6544b1e8223fbfb31d5e52
PedroBemerguy <pedrobemerguy96@gmail.com>
2019-10-23 18:11:44 -0300

    Fix time sync bugs

--- functions/timeSync.m
+++ functions/timeSync.m
@@ -1,4 +1,5 @@
-function [ rxSymbols ] = timeSync( preambledSymbols, frameSize, txPreamble, upFactor)
+function [ rxSymbols ] = timeSync( preambledSymbols, frameSize, ...
+                                   txPreamble, upFactor, mf_delay)
 %timeSync Summary of this function:
 %   Goal: Given the number of samples per frame, this function synchronizes
 %   in time the received signal
@@ -6,6 +7,8 @@ function [ rxSymbols ] = timeSync( preambledSymbols, frameSize, txPreamble, upFa
 %   - preambledSymbols: the received signal that needs symbol synchronization.
 %   - txPreamble: the preambles transmitted by the transmitter.
 %   - frameSize: how much samples to each frame.
+%   - upFactor: the oversampling factor from the preambledSymbols.
+%   - mf_delay: the delay in symbols that the matched filter adds.
 %   Output:
 %   - preambledSignal: the normalized signal with a preamble in each frame.
 %   - txPreamble: the preamble signal in symbols
@@ -22,27 +25,32 @@ function [ rxSymbols ] = timeSync( preambledSymbols, frameSize, txPreamble, upFa
         if (detectedLags(i+1) - detectedLags(i) > frameSize*upFactor)
             previousLastLag = lastLag;
             lastLag = i;
-            lagsRange = find(lags >= detectedLags(previousLastLag + 1) & ...
-                             lags <= detectedLags(lastLag));
+            if(lastLag == previousLastLag)
+                lagsRange = find(lags >= detectedLags(previousLastLag) & ...
+                                 lags <= detectedLags(lastLag));
+            else
+                lagsRange = find(lags >= detectedLags(previousLastLag+1) & ...
+                                 lags <= detectedLags(lastLag));
+            end
             framesLag(j) = lags(find(abs(c(lagsRange)) == ...
                                 max(abs(c(lagsRange))))+lagsRange(1)-1);
             j = j + 1;
         end
     end 
-    lagsRange = find(lags > detectedLags(lastLag + 1) & ...
-                     lags < detectedLags(end));
+    lagsRange = find(lags >= detectedLags(lastLag + 1) & ...
+                     lags <= detectedLags(end));
     framesLag(j) = lags(find(abs(c(lagsRange)) == ...
                         max(abs(c(lagsRange))))+lagsRange(1)-1);
-    framesLag = nonzeros(framesLag);
+    framesLag = [ framesLag(1:j-1)' nonzeros(framesLag(j:end))];
     
     % Read the symbols from each frame
     nFrames = length(framesLag);
     symbols = zeros(frameSize*nFrames*upFactor,1);
     for i = 1:nFrames
         if (i == nFrames)
-            final = length(preambledSymbols((framesLag(i)+1+upFactor*preambleSize):end));
+            final = length(preambledSymbols((framesLag(i)+1+upFactor*preambleSize):(end-2*mf_delay*upFactor)));
             symbols((1+(i-1)*frameSize*upFactor):((i-1)*frameSize*upFactor + final)) = ...
-            preambledSymbols((framesLag(i)+1+upFactor*preambleSize):end);
+            preambledSymbols((framesLag(i)+1+upFactor*preambleSize):(end-2*mf_delay*upFactor));
         else
             symbols((1+(i-1)*frameSize*upFactor):(i*(frameSize)*upFactor+1-upFactor)) = ...
             preambledSymbols(framesLag(i)+1+upFactor*preambleSize:...
