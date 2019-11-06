e6577f5d617c579a2667009c3f30d11057c38fd6
Cleverson Nahum <cleversonahum@gmail.com>
2019-11-06 18:04:00 -0300

    Update how images are choose on receiver

--- receiver.m
+++ receiver.m
@@ -19,6 +19,32 @@ en_channel = 1; % Flag to Enable Channel
 channel_code_type = 'conv'; % 'conv' or 'linear'
 img_name = 'images/5.1.10.tiff';
 input_audio = './transmit_audio.wav';
+img_number = 9; % 9, 10, 11, 12, 13 or 14
+
+switch img_number
+    case 9
+        img_props.filename = 'images/5.1.09.tiff';
+        img_props.source_size = 98192;
+    case 10
+        img_props.filename = 'images/5.1.10.tiff';
+        img_props.source_size = 170036;
+    case 11
+        img_props.filename = 'images/5.1.11.tiff';
+        img_props.source_size = 98462;
+    case 12
+        img_props.filename = 'images/5.1.12.tiff';
+        img_props.source_size = 125322;
+    case 13
+        img_props.filename = 'images/5.1.13.tiff';
+        img_props.source_size = 217301;
+    case 14
+        img_props.filename = 'images/5.1.14.tiff';
+        img_props.source_size = 141780;
+    otherwise
+        disp('Choose a valid image (9, 10, 11, 12, 13 or 14)')
+        return
+        
+end
 
 %% Receiver
 
@@ -64,7 +90,7 @@ decoded_rx_bitstream = channelDecoding(rx_bitstream(1:n_frames*S*log2(M)),...
 
 
 % Source Decoding and recovering image
-img = sourceDecoding(decoded_rx_bitstream(1:170036));
+img = sourceDecoding(decoded_rx_bitstream(1:img_props.source_size));
 
 % Calculating PSNR
 ori_img = imread(img_name);
